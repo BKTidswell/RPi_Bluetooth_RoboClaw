36bc830e09c7b39c0127be9bad8609cd6a27150d
BKTidswell <ben.tidswell@tufts.edu>
2018-12-18 17:50:58 -0500

    debugging done. holding buttons does not work.

--- bt_control_motors.py
+++ bt_control_motors.py
@@ -30,10 +30,10 @@ gamepad = InputDevice('/dev/input/event0')
 
 #Opens up roboclaw inputs
 rc1 = Roboclaw("/dev/ttyACM0",9600)
-rc2 = Roboclaw("/dev/ttyACM1",9600)
+#rc2 = Roboclaw("/dev/ttyACM1",9600)
 
 rc1.Open()
-rc2.Open()
+#rc2.Open()
 
 address = 0x80
 
@@ -46,8 +46,8 @@ yBtn = 308
 start = 315
 select = 314
 
-lTrig = 311
-rTrig = 310
+lTrig = 310
+rTrig = 311
 
 #These aren't button numbers but are used for the D-pad
 # which uses a different system
@@ -73,114 +73,125 @@ left = 3
 #creates motor winding functions
 #forwards
 def motorWind(mNum,speed):
-	if mNum = 1:
-		rc1.ForwardM1(address,speed*spdMax)
-	elif mNum = 2:
-		rc1.ForwardM2(address,speed*spdMax)
-	elif mNum = 3:
-		rc2.ForwardM1(address,speed*spdMax)
+	if mNum == 1:
+		rc1.ForwardM1(address,int(speed*spdMax))
+	elif mNum == 2:
+		rc1.ForwardM2(address,int(speed*spdMax))
+	elif mNum == 3:
+		#rc2.ForwardM1(address,speed*spdMax)
+		pass
 	else:
 		print("Incorrect Motor Number")
 
 #backwards
 def motorUnwind(mNum,speed):
-	if mNum = 1:
-		rc1.BackwardM1(address,speed*spdMax)
-	elif mNum = 2:
-		rc1.BackwardM2(address,speed*spdMax)
-	elif mNum = 3:
-		rc2.BackwardM1(address,speed*spdMax)
+	if mNum == 1:
+		rc1.BackwardM1(address,int(speed*spdMax))
+	elif mNum == 2:
+		rc1.BackwardM2(address,int(speed*spdMax))
+	elif mNum == 3:
+		#rc2.BackwardM1(address,speed*spdMax)
+		pass
 	else:
 		print("Incorrect Motor Number")
 
 #stopped
-def motorStop(nNum):
-	if mNum = 1:
+def motorStop(mNum):
+	if mNum == 1:
 		rc1.ForwardM1(address,0)
-	elif mNum = 2:
+	elif mNum == 2:
 		rc1.ForwardM2(address,0)
-	elif mNum = 3:
-		rc2.ForwardM1(address,0)
+	elif mNum == 3:
+		#rc2.ForwardM1(address,0)
+		pass
 	else:
 		print("Incorrect Motor Number")
 
 #loop and filter by event code and print the mapped label
-for event in gamepad.read_loop():
-	if event.type == ecodes.EV_KEY:
-		if event.value == 1:
-			if event.code == yBtn:
-				#Turn Left
-				motorUnwind(top,0.5)
-				motorUnwind(right,1)
-				motorWind(left,1)
-				print("Y")
-			elif event.code == bBtn:
-				#Inch
-				motorUnwind(top,1)
-				motorWind(right,1)
-				motorWind(left,1)
-				print("B")
-			elif event.code == aBtn
-				#Unwind
-				motorUnwind(top,0.5)
-				motorUnwind(right,0.5)
-				motorUnwind(left,0.5)
-				print("A")
-			elif event.code == xBtn:
-				#Turn Right
-				motorUnwind(top,0.5)
-				motorWind(right,1)
-				motorUnwind(left,1)
-				print("X")
 
-			elif event.code == start:
-				#E-Stop
-				motorStop(top)
-				motorStop(right)
-				motorStop(left)
-				print("start")
-			elif event.code == select:
-				#Motor Reset?
-				print("select")
-
-			elif event.code == lTrig:
-				#Rotate Contact Left (Counterclock)
-				temp = top
-				top = right
-				right = left
-				left = top
-				print("left bumper")
-
-			elif event.code == rTrig:
-				#Rotate Contact Right (Clock)
-				temp = top
-				top = left
-				left = right
-				right = temp
-				print("right bumper")
-   
-	elif event.type == ecodes.EV_ABS:    
-		if event.code == y:
-			if event.value == up:
-				#Lift
-				motorWind(top,1)
-				motorUnwind(right,1)
-				motorUnwind(left,1)
-				print("up")
-			elif event.value == down:
-				#?????
-				print("down")
-		elif event.code == x:
-			if event.value == left:
-				#Move Left
-				print("left")
-			elif event.value == right:
-				#Move Right
-				print("right")
-	else:
-		motorStop(top)
-		motorStop(right)
-		motorStop(left)
+try:
+	for event in gamepad.read_loop():
+		if event.type == ecodes.EV_KEY:
+			if event.value == 1:
+				if event.code == yBtn:
+					#Turn Left
+					motorUnwind(top,0.5)
+					motorUnwind(right,1)
+					motorWind(left,1)
+					print("Y")
+				elif event.code == bBtn:
+					#Inch
+					motorUnwind(top,1)
+					motorWind(right,1)
+					motorWind(left,1)
+					print("B")
+				elif event.code == aBtn:
+					#Unwind
+					motorUnwind(top,0.5)
+					motorUnwind(right,0.5)
+					motorUnwind(left,0.5)
+					print("A")
+				elif event.code == xBtn:
+					#Turn Right
+					motorUnwind(top,0.5)
+					motorWind(right,1)
+					motorUnwind(left,1)
+					print("X")
+	
+				elif event.code == start:
+					#E-Stop
+					motorStop(top)
+					motorStop(right)
+					motorStop(left)
+					print("start")
+				elif event.code == select:
+					#Motor Reset?
+					print("select")
+	
+				elif event.code == lTrig:
+					#Rotate Contact Left (Counterclock)
+					temp = top
+					top = right
+					right = left
+					left = top
+					print("left bumper")
+	
+				elif event.code == rTrig:
+					#Rotate Contact Right (Clock)
+					temp = top
+					top = left
+					left = right
+					right = temp
+					print("right bumper")
 	   
+		elif event.type == ecodes.EV_ABS:    
+			if event.code == y:
+				if event.value == up:
+					#Lift
+					motorWind(top,1)
+					motorUnwind(right,1)
+					motorUnwind(left,1)
+					print("up")
+				elif event.value == down:
+					#?????
+					print("down")
+			elif event.code == x:
+				if event.value == left:
+					#Move Left
+					print("left")
+				elif event.value == right:
+					#Move Right
+					print("right")
+		#else:
+		#	time.sleep(0.1)
+		#	motorStop(top)
+		#	motorStop(right)
+		#	motorStop(left)
+
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+	motorStop(top)
+	motorStop(right)
+	motorStop(left) 
+   
 
 
--- serialMotor.py
+++ serialMotor.py
@@ -9,15 +9,15 @@ rc = Roboclaw("/dev/ttyACM0",9600)
 rc.Open()
 address = 0x80
 
-rc.ForwardM1(address,127) #full forwards
+rc.ForwardM2(address,127) #full forwards
 print "running forward"
 time.sleep(3)
-rc.ForwardM1(address,0) #stopped
+rc.ForwardM2(address,0) #stopped
 print "stopped"
-rc.BackwardM1(address,127) #full back
+rc.BackwardM2(address,127) #full back
 print "running backwards"
 time.sleep(3)
-rc.ForwardM1(address,0) #stopped
+rc.ForwardM2(address,0) #stopped
 print "stopped"
 
 rc.ForwardM1(address,64) #half forwards
